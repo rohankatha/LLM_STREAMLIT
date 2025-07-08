06f8d4d998aa9d5fdfcce8ed4bd7ed540932d89c
katha rohan reddy <rohan.katha@students.iiit.ac.in>
2025-07-08 21:35:16 +0530

    Update Hugging_LLM.py

--- Hugging_LLM.py
+++ Hugging_LLM.py
@@ -3,11 +3,13 @@ from huggingface_hub import InferenceClient
 import streamlit as st
 
 
-HF_TOKEN = OS.GET_TOKEN()
+#HF_TOKEN = OS.GET_TOKEN()
 
-model = InferenceClient(api_key=HF_TOKEN)
+
 st.title("Image to text converter")
 inoput = st.text_input(placeholder="Enter the prompt :) ",label="LLMPROMPT")
+huggin_token = st.sidebar.text_input("Enter the Hugging face token")
+model = InferenceClient(api_key=huggin_token)
 if inoput:
  Img = model.text_to_image(inoput,model="black-forest-labs/FLUX.1-dev")
  string_name = inoput+".png"
