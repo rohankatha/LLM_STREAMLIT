85aedaf8912232dee6d781239995c4ec6cba1446
katha rohan reddy <rohan.katha@students.iiit.ac.in>
2025-07-08 20:45:58 +0530

    Add files via upload

---
+++ Hugging_LLM.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from huggingface_hub import InferenceClient
+import streamlit as st
+
+
+HF_TOKEN = OS.GET_TOKEN()
+
+model = InferenceClient(api_key=HF_TOKEN)
+st.title("Image to text converter")
+inoput = st.text_input(placeholder="Enter the prompt :) ",label="LLMPROMPT")
+if inoput:
+ Img = model.text_to_image(inoput,model="black-forest-labs/FLUX.1-dev")
+ string_name = inoput+".png"
+ #Img.save(fp=string_name)
+ st.image(Img)
